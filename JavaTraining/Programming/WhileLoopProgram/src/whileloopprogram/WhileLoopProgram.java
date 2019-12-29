52b5f899209a92f290faf6722a78d602dbedef1a
Kaiserin_der_Welt <42034666+KaiserinDerWelt@users.noreply.github.com>
2019-12-29 17:42:24 -0600

    Add files via upload

---
+++ JavaTraining/Programming/WhileLoopProgram/src/whileloopprogram/WhileLoopProgram.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package whileloopprogram;
+
+/**
+ *
+ * @author marianalicona
+ */
+public class WhileLoopProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Initialize variable
+      int numberOfDogs = 0;
+
+while (numberOfDogs < 10) {
+    numberOfDogs += 1;
+    System.out.println("I adopted " + numberOfDogs + " dogs");
+}
+
+System.out.println("I have a dog zoo!");
+        
+        }
+    }
+    

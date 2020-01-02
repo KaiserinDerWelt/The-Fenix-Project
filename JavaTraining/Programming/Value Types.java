0be77189082f6bd6e5f859ad9431c3c97bcadcde
Kaiserin_der_Welt <42034666+KaiserinDerWelt@users.noreply.github.com>
2020-01-01 21:21:51 -0600

    Create Value Types.java

---
+++ JavaTraining/Programming/Value Types.java
@@ -0,0 +1,13 @@
+//Example No.1
+// declare variable a as an int and initialize it with a value of 10
+int a = 10;
+// declare variable b as an int and initialize it with a copy of the value of a
+int b = a;
+
+//Example Two
+
+int CatsInBoxOne=10;
+int CatsInboxTwo=21;
+int TotalCats= CatsInBoxOne + CatsInBoxTwo;
+System.out.PrintIn(CatsInBoxOne +CatsInBoxTwo); //Print value types operation.
+System.out.PrintIn(TotalCats); //Print Variable operation.
